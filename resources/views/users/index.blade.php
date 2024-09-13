6223bc64c381c8ae9340ef0423d432d4a7733fee
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-09-13 16:17:10 +0300

    User layouts

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('body')
     <table border=1>
         <thead>
             <th>Name</th>
@@ -17,10 +11,8 @@
         <tr>
             <td>{{ $user->name }}</td>
             <td>{{ $user->email }}</td>
-            {{-- <td><a href={{ route('show', ['id' => $user->id]) }}>SHOW</a></td> --}}
+            <td><a href={{ route('users.show', $user->id) }}>SHOW</a></td>
         </tr>
         @endforeach
     </table>
-    
-</body>
-</html>
+@endsection
