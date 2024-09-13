30cea23317540f64c3189b6f9b407752cd5411e3
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-09-13 12:23:39 +0300

    Check routing base

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return  view('welcome');
 });
+
+Route::get('/string', function () {
+    return  "string";
+});
+
+//with params
+Route::get('/hello/{name}', function($name){
+    return "Hello $name";
+});
+
+//with params not mandatory
+Route::get('/user/{name?}', function($name = 'Nant-Default'){
+    return "Hello $name";
+});
+
+//with params and validation type
+Route::get('/user-valid/{name}', function($name){
+    return "Hello $name";
+})->where('name', '[A-Za-z]+');
